8e3e5019308ffdfcfd39c934d1afef3000e83694
Resine <resine@RESMAC01.local>
2023-10-31 12:27:29 +0100

    weather setup

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -1,49 +1,23 @@
 import React, { useEffect, useState } from "react";
-import Clear from "../../assets/weather-icons/clear.png";
-import Cloud from "../../assets/weather-icons/cloud.png";
-import Drizzle from "../../assets/weather-icons/drizzle.png";
-import Humidity from "../../assets/weather-icons/humidity.png";
-import Rain from "../../assets/weather-icons/rain.png";
-import Snow from "../../assets/weather-icons/snow.png";
-import Wind from "../../assets/weather-icons/wind.png";
+import {WiHumidity} from 'react-icons/wi';
+import {TiWeatherSunny,TiWeatherNight ,TiWeatherCloudy,TiWeatherShower,TiWeatherSnow, TiWeatherPartlySunny, TiWeatherDownpour, TiWeatherWindy} from "react-icons/ti"
 import "./weather.css";
 
 function Weather() {
   const [weatherData, setWeatherData] = useState(null);
-  const [wicon, setWicon] = useState(Clear);
+ 
 
   const fetchWeatherData = async () => {
-    const apiKey = "83c3cee8d38f7fbc3fd7c0afbf7b118b"; // Replace with your OpenWeatherMap API key
-    // const lon = "5.928";
-    // const lat = "43.124228";
-    const apiUrl = `http://localhost:3001/data/2.5/weather?q=Lyon,fr&units=metric&appid=${apiKey}`;
+    const apiKey = "83c3cee8d38f7fbc3fd7c0afbf7b118b"; 
+   
+    const apiUrl = `http://localhost:3001/data/2.5/weather?q=Toulon,fr&units=metric&appid=${apiKey}`;
 
     try {
       const response = await fetch(apiUrl);
       if (response.ok) {
         const data = await response.json();
         setWeatherData(data);
-        // Check the weather icon and update wicon accordingly
-        if (data.weather && data.weather.length > 0) {
-          const icon = data.weather[0].icon;
-          if (icon === "01d" || icon === "01n") {
-            setWicon(Clear);
-          } else if (icon === "02d" || icon === "02n") {
-            setWicon(Cloud);
-          } else if (icon === "03d" || icon === "03n") {
-            setWicon(Drizzle);
-          } else if (icon === "04d" || icon === "04n") {
-            setWicon(Drizzle);
-          } else if (icon === "09d" || icon === "09n") {
-            setWicon(Rain);
-          } else if (icon === "10d" || icon === "10n") {
-            setWicon(Rain);
-          } else if (icon === "13d" || icon === "13n") {
-            setWicon(Snow);
-          } else {
-            setWicon(Clear);
-          }
-        }
+    
       } else {
         console.error(
           "Failed to fetch weather data. Check your API key and endpoint."
@@ -57,21 +31,89 @@ function Weather() {
   useEffect(() => {
     fetchWeatherData();
   }, []);
+  const timestamp = 1698738641; 
+const date = new Date(timestamp * 1000); 
 
+const formattedDate = date.toLocaleDateString(); 
+
+console.log(formattedDate); 
+
+   
   return (
-    <div className="weather-widget">
+    <div className="weather-widget ">
       {weatherData ? (
         <div className="weather-container">
+          <div className="weather-location"> <h4 > Météo à {weatherData.name}</h4>
+          <p> {formattedDate}</p></div>
+         <div className="weather-data"> 
+         
+          <div className="data-container">
+          {weatherData.weather[0].icon === "01d" ? (
+  <TiWeatherSunny className="weather-icon" />
+) : (
+  weatherData.weather[0].icon === "01n" ? <TiWeatherNight className="weather-icon"/> : null
+)}
+          {weatherData.weather[0].icon === "02d" ? (
+  <TiWeatherPartlySunny className="weather-icon"/>
+) : (
+  weatherData.weather[0].icon === "02n" ? <TiWeatherCloudy  className="weather-icon"/> : null
+)}
+  {weatherData.weather[0].icon === "03d"  ||  weatherData.weather[0].icon === "03n"  ? (
+   <TiWeatherDownpour className="weather-icon"/>
+) 
+ : null
+}
+{weatherData.weather[0].icon === "09d"  ||  weatherData.weather[0].icon === "09n"  || weatherData.weather[0].icon === "10d"  ||  weatherData.weather[0].icon === "10n"  ? (
+  <TiWeatherShower className="weather-icon" /> 
+) 
+ : null
+}
+{weatherData.weather[0].icon === "04d"  ||  weatherData.weather[0].icon === "04n"  ? (
+   <TiWeatherDownpour className="weather-icon"/>
+) 
+ : null
+}
+{weatherData.weather[0].icon === "13d"  ||  weatherData.weather[0].icon === "13n"  ? (
+   <TiWeatherSnow className="weather-icon"/>
+) 
+ : null
+}
+
+          <div className="weather-temp">
+            <p className="weather-value">
+          {Math.floor(weatherData.main.temp)} °C
+          </p>
+          </div>
+          
+
+          </div>
+         
+          <div className="data-container">
           <div className="weather-image">
-            <img src={wicon} alt="" />
+     <TiWeatherWindy className="weather-icon"/>
           </div>
           <div className="weather-temp">
-            {Math.floor(weatherData.main.temp)} °C
+            <p className="weather-value">
+            {Math.floor(weatherData.wind.speed)} km/h
+            </p>
+            
           </div>
-          <div className="weather-location">{weatherData.name}</div>
+         
+           
+          </div>
           <div className="data-container">
-            {/* Add other weather data elements here */}
+          <div className="weather-image">
+         <WiHumidity className="weather-icon"/>
           </div>
+          <div className="weather-temp">
+            <p className="weather-value">
+            {Math.floor(weatherData.main.humidity)} %
+            </p>
+            
+          </div>
+           
+           </div>
+           </div>
         </div>
       ) : (
         <p>Loading weather data...</p>
